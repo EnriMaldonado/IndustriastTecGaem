4d41bd1c56496c768fad4080e50910129c37dc77
Enrique <enriquemaldonado458@gmail.com>
2022-07-07 19:52:27 -0600

    Agregando controlador.clase BL para ordenes

--- IndustriasTecGaem/IndustriasTecGaem.BL/Producto.cs
+++ IndustriasTecGaem/IndustriasTecGaem.BL/Producto.cs
@@ -10,20 +10,17 @@ namespace IndustriasTecGaem.BL
     public class Producto
     {
         public int Id { get; set; }
-
         [Required(ErrorMessage = "Ingrese la descripcion")]
         [MinLength(3, ErrorMessage = "Ingrese minimo 3 Caracteres")]
         [MaxLength(20, ErrorMessage = "Ingrese maximo de 20 Caracteres")]
         public string Descripcion { get; set; }
-
         [Required(ErrorMessage = "Ingrese el precio")]
-        [Range(0, 10000, ErrorMessage = "Ingrese un precio entre 0 y 10000")]
+        // [Range(0, 1000, ErrorMessage = "Ingrese un precio entre 0 y 10000")]
         public double Precio { get; set; }
         public int CategoriaId { get; set; }
-
         [Display(Name = "Imagen" )]
         public string UrlImagen { get; set; }
-
+        //agregados de categoria
         public Categorias categoria { get; set; }
         public bool Activo { get; set; }
     }
